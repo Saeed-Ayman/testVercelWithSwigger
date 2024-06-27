9a8dd4928e00a48daaa683a036a1136e20434b09
saeed-ayman <saeed20359@gmail.com>
2024-06-27 19:15:49 +0300

    continue with threshold

--- routes/api.php
+++ routes/api.php
@@ -12,4 +12,9 @@ Route::get("/users/{id}", [\App\Http\Controllers\UserController::class, 'show'])
 Route::post("/users", [\App\Http\Controllers\UserController::class, 'store']);
 Route::put("/users/{id}", [\App\Http\Controllers\UserController::class, 'update']);
 Route::delete("/users/{id}", [\App\Http\Controllers\UserController::class, 'destroy']);
+
 Route::get("/test", [\App\Http\Controllers\TestController::class, 'index']);
+Route::get("/test/{id}", [\App\Http\Controllers\TestController::class, 'show']);
+Route::post("/test", [\App\Http\Controllers\TestController::class, 'store']);
+Route::put("/test/{id}", [\App\Http\Controllers\TestController::class, 'update']);
+Route::delete("/test/{id}", [\App\Http\Controllers\TestController::class, 'destroy']);
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::redirect('/', 'api/documentation');
 
 Route::get("/test", [\App\Http\Controllers\TestController::class, 'index']);
+Route::get("/test/{id}", [\App\Http\Controllers\TestController::class, 'show']);
+Route::post("/test", [\App\Http\Controllers\TestController::class, 'store']);
+Route::put("/test/{id}", [\App\Http\Controllers\TestController::class, 'update']);
+Route::delete("/test/{id}", [\App\Http\Controllers\TestController::class, 'destroy']);
