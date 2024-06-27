b5be1971ef57100f214e1ffad78f4e31ebe16deb
saeed-ayman <saeed20359@gmail.com>
2024-06-27 19:19:36 +0300

    continue with threshold

--- routes/api.php
+++ routes/api.php
@@ -7,8 +7,8 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get("/test", [\App\Http\Controllers\UserController::class, 'index']);
-Route::get("/test/{id}", [\App\Http\Controllers\UserController::class, 'show']);
-Route::post("/test", [\App\Http\Controllers\UserController::class, 'store']);
-Route::put("/test/{id}", [\App\Http\Controllers\UserController::class, 'update']);
-Route::delete("/test/{id}", [\App\Http\Controllers\UserController::class, 'destroy']);
+Route::get("/users", [\App\Http\Controllers\UserController::class, 'index']);
+Route::get("/users/{id}", [\App\Http\Controllers\UserController::class, 'show']);
+Route::post("/users", [\App\Http\Controllers\UserController::class, 'store']);
+Route::put("/users/{id}", [\App\Http\Controllers\UserController::class, 'update']);
+Route::delete("/users/{id}", [\App\Http\Controllers\UserController::class, 'destroy']);
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', 'api/documentation');
 
-Route::get("/test", [\App\Http\Controllers\UserController::class, 'index']);
-Route::get("/test/{id}", [\App\Http\Controllers\UserController::class, 'show']);
-Route::post("/test", [\App\Http\Controllers\UserController::class, 'store']);
-Route::put("/test/{id}", [\App\Http\Controllers\UserController::class, 'update']);
-Route::delete("/test/{id}", [\App\Http\Controllers\UserController::class, 'destroy']);
+Route::get("/users", [\App\Http\Controllers\UserController::class, 'index']);
+Route::get("/users/{id}", [\App\Http\Controllers\UserController::class, 'show']);
+Route::post("/users", [\App\Http\Controllers\UserController::class, 'store']);
+Route::put("/users/{id}", [\App\Http\Controllers\UserController::class, 'update']);
+Route::delete("/users/{id}", [\App\Http\Controllers\UserController::class, 'destroy']);
