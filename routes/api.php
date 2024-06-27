d26dab54b46c975a1cc520ab7680272b2ded5c5e
saeed-ayman <saeed20359@gmail.com>
2024-06-27 18:57:36 +0300

    add test to solve problem

--- routes/api.php
+++ routes/api.php
@@ -12,3 +12,4 @@ Route::get("/users/{id}", [\App\Http\Controllers\UserController::class, 'show'])
 Route::post("/users", [\App\Http\Controllers\UserController::class, 'store']);
 Route::put("/users/{id}", [\App\Http\Controllers\UserController::class, 'update']);
 Route::delete("/users/{id}", [\App\Http\Controllers\UserController::class, 'destroy']);
+Route::get("/test", [\App\Http\Controllers\TestController::class, 'index']);
--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', 'api/documentation');
+
+Route::get("/test", [\App\Http\Controllers\TestController::class, 'index']);
