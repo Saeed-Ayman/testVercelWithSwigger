0bd6c260c82dfeacdd3cce5acb35e95767fa7bd8
saeed-ayman <saeed20359@gmail.com>
2024-06-27 17:25:33 +0300

    deploy

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get("/test", fn() => "test");
+Route::redirect('/', '/api/documentation');
 
-Route::redirect('/', 'api/documentation');
+Route::get("/api/users", [\App\Http\Controllers\UserController::class, 'index']);
+Route::get("/api/users/{id}", [\App\Http\Controllers\UserController::class, 'show']);
+Route::post("/api/users", [\App\Http\Controllers\UserController::class, 'store']);
+Route::put("/api/users/{id}", [\App\Http\Controllers\UserController::class, 'update']);
+Route::delete("/api/users/{id}", [\App\Http\Controllers\UserController::class, 'destroy']);
