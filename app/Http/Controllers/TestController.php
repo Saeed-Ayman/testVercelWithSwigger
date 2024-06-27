b012bd00750290641c1431f27d1e911a1d4033c0
saeed-ayman <saeed20359@gmail.com>
2024-06-27 19:10:50 +0300

    continue with threshold

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
     public function index()
     {
-        return PHP_VERSION;
+        return User::all();
     }
 }
